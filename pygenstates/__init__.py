d18e941f37f60fcc56a05e37d754edd205782592
gflower480 <68313658+gflower480@users.noreply.github.com>
2026-05-01 15:03:10 +0800

    Forgot to build

--- pygenstates/__init__.py
+++ pygenstates/__init__.py
@@ -2,6 +2,6 @@
 
 from .eigensolver import Ceigensolver, available_methods, eigensolver
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = ["eigensolver", "Ceigensolver", "available_methods", "__version__"]
